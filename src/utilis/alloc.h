eac4b1020b5dfd7b5d9b3b98863d4d2b3edb6c98
Mitrofan <mitrofan3452@gmail.com>
2025-04-16 14:37:09 +0300

    allocation table for debug

--- src/utilis/alloc.h
+++ src/utilis/alloc.h
@@ -5,35 +5,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
-// use counters to debug if memory leaks 
-struct {
+#ifdef ALLOCATION_DEBUG
+static struct {
     size_t total_allocations;
-    size_t current_allocations;
+    size_t table_size;
+    void** pp_table;
 } alloc_info = {
     .total_allocations = 0,
-    .current_allocations = 0
+    .table_size = 0,
+    .pp_table = NULL
 };
+
+bool allocFindCell(const void* val, size_t* p_id) {
+    for(size_t i = 0; i < alloc_info.table_size; i++) {
+        if(alloc_info.pp_table[i] == val) {
+            *p_id = i;
+            return true;
+        }
+    }
+    return false;
+}
+
+void allocTableInit(
+    size_t size
+) {
+    alloc_info.table_size = size;
+    alloc_info.pp_table = (void**)malloc(size * sizeof(void*));
+    for(size_t i = 0; i < alloc_info.table_size; i++) {
+        alloc_info.pp_table[i] = NULL;
+    }
+}
+
+void allocTableTerminate(void) {
+    size_t count = 0;
+    for(size_t i = 0; i < alloc_info.table_size; i++) {
+        count += (size_t)(alloc_info.pp_table[i] != NULL);        
+    }
+    printf("allocations left: %llu \n", count);
+    free(alloc_info.pp_table);
+}
+#endif
 
 // malloc with allocations tracking
 void* allocMalloc(size_t bytes) {
     void* p = malloc(bytes);
+#ifdef ALLOCATION_DEBUG
+    size_t id;
+    if(allocFindCell(NULL, &id)) {
+        alloc_info.pp_table[id] = p;
+    }
     alloc_info.total_allocations++;
-    alloc_info.current_allocations++;
+#endif
     return p;
 }
 
 // realloc with allocations tracking
 void* allocRealloc(void* p, size_t size) {
     void* p_new = realloc(p, size);
+#ifdef ALLOCATION_DEBUG
+    size_t id;
+    if(allocFindCell(p, &id)) {
+        alloc_info.pp_table[id] = NULL;
+    }
+    if(allocFindCell(NULL, &id)) {
+        alloc_info.pp_table[id] = p_new;
+    }
     alloc_info.total_allocations++;
+#endif
     return p_new;
 }
 
 //free with allocations tracking
 void allocFree(void* p) {
+#ifdef ALLOCATION_DEBUG
+    size_t id;
+    if(allocFindCell(p, &id)) {
+        alloc_info.pp_table[id] = NULL;
+    }
+#endif
     free(p);
-    alloc_info.current_allocations--;
 }
 
 #endif
